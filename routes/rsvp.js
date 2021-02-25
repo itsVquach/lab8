4e5ffb95105f0078891851836500e769a7b0bc6d
Vivian Quach <vquach@ucsd.edu>
2021-02-25 10:07:57 -0800

    Finished Lab 8 Early

--- routes/rsvp.js
+++ routes/rsvp.js
@@ -8,3 +8,14 @@ var data = {
 exports.adminView = function(request, response){
   response.render('rsvp', data);
 };
+
+exports.addRSVP = function(request, response){
+  var rsvpEmail = request.body.rsvpEmail;
+  //This will print to your terminal whenever the POST request is made
+  console.log(rsvpEmail);
+
+  data.rsvp.push(rsvpEmail); //Add to current data
+
+  //Send back some data so browser knows itworked
+  response.send(rsvpEmail);
+};
