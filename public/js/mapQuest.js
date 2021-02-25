579f644ec3bb7d322ea122932cf12f1b76173247
Vivian Quach <vquach@ucsd.edu>
2021-02-25 10:15:42 -0800

    Fixed map coordinates to SSC

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -4,7 +4,7 @@ function initMap() {
 
 // 'map' refers to a <div> element with the ID map
 	L.mapquest.map('map', {
-	  center: [37.7749, -122.4194],
+	  center: [32.8785, -117.2359],
 	  layers: L.mapquest.tileLayer('map'),
 	  zoom: 12
 	});
